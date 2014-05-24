3d57fbe9244d863e8b3df866b2e0a7e9e61b83b5
T107 <T107@PC267>
2014-05-23 21:35:06 -0500

    mi practica de hoy

---
+++ src/main/java/porras_proyecto1/ContadorVisitas.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package porras_proyecto1;
+
+
+
+public class ContadorVisitas {
+    
+    private static int contador;
+
+    public static int getContador() {
+        contador++;
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+ 
+ 
+
+ 
+
+
+ 
+
+ 
+    
+    
+    
+    
+    
+}
